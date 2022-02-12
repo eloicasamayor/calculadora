d25e18ce1e4c67d3aab4837ce8f9194972bb0105
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2022-02-12 20:07:56 +0100

    Updates

--- src/Components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button({ classe, valor, funcio }) {
-  return (
-    <button className={"btn-" + classe} onClick={() => funcio(valor)}>
-      {valor}
-    </button>
-  );
-}
--- src/Components/ClearButton.js
+++
@@ -1,7 +0,0 @@
-export default function ClearButton({ funcio }) {
-  return (
-    <button onClick={() => funcio()} className="clearButton">
-      Clear
-    </button>
-  );
-}
